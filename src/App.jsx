0fec9e9eaab941106711d187cde3337eeeef4861
blankcanvasdesignco <sumit@panoslice.com>
2025-08-21 10:34:52 +0530

    back to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
 import Contact from "./pages/Contact";
 import Services from "./pages/Services";
-import CanvasEditor from "./components/CanvasEditor";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/services" element={<Services />} />
-          {/* <Route path="/canvas" element={<CanvasEditor />} /> */}
         </Routes>
         <Footer />
       </div>
